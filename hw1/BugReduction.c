dc7afe86bbbdc4f4abf3a15b70a4130706ca6263
Alexander Varlamov <varlamow.sas@gmail.com>
2026-04-13 15:17:07 +0300

    BugReduction task done

--- hw1/BugReduction.c
+++ hw1/BugReduction.c
@@ -2,42 +2,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-float dotprod(float * a, float * b, size_t N)
+float dotprod(float *a, float *b, size_t N)
 {
-    int i, tid;
-    float sum;
+    int i;
+    float sum = 0.0f;
 
-    tid = omp_get_thread_num();
-
-    #pragma omp for reduction(+:sum)
-    for (i = 0; i < N; ++i)
+#pragma omp parallel for reduction(+ : sum)
+    for (i = 0; i < (int)N; ++i)
     {
         sum += a[i] * b[i];
-        printf("tid = %d i = %d\n", tid, i);
+        printf("tid = %d i = %d\n", omp_get_thread_num(), i);
     }
 
     return sum;
 }
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
     const size_t N = 100;
     int i;
     float sum;
     float a[N], b[N];
 
-
-    for (i = 0; i < N; ++i)
+    for (i = 0; i < (int)N; ++i)
     {
-        a[i] = b[i] = (double)i;
+        a[i] = b[i] = (float)i;
     }
 
-    sum = 0.0;
+    sum = dotprod(a, b, N);
 
-    #pragma omp parallel shared(sum)
-    dotprod(a, b, N);
-
-    printf("Sum = %f\n",sum);
+    printf("Sum = %f\n", sum);
 
     return 0;
 }
---
+++ scripts/bugs.sh
@@ -0,0 +1,6 @@
+cd hw1
+
+## 1:
+gcc-15 BugReduction.c -o bugreduction -fopenmp
+./bugreduction
+rm bugreduction
