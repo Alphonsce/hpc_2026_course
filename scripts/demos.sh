dfdd16d6f268d9f12851c2baee0a8af18d124bcf
Alexander Varlamov <varlamow.sas@gmail.com>
2026-04-13 15:16:54 +0300

    finished working with demos

--- scripts/demos.sh
+++ scripts/demos.sh
@@ -1,20 +1,27 @@
-#!/usr/bin/env bash
-
 cd demos
-libomp_path=/opt/homebrew/opt/libomp
 
 ## 1:
-gcc-15 Hello.c -o hello -fopenmp
-OMP_NUM_THREADS=4 ./hello
-rm hello
+# gcc-15 Hello.c -o hello -fopenmp
+# OMP_NUM_THREADS=4 ./hello
+# rm hello
 
 ## 2:
-g++-15 -std=c++17 -fopenmp OutMes.cpp -o outmes
-OMP_NUM_THREADS=4 ./outmes
-./outmes
-rm outmes
+# g++-15 -std=c++17 -fopenmp OutMes.cpp -o outmes
+# OMP_NUM_THREADS=4 ./outmes
+# ./outmes
+# rm outmes
 
 ## 3:
-gcc-15 PrivateShared.c -o private_shared -fopenmp
-OMP_NUM_THREADS=4 ./private_shared
-rm private_shared
+# gcc-15 PrivateShared.c -o private_shared -fopenmp
+# OMP_NUM_THREADS=4 ./private_shared
+# rm private_shared
+
+## 4:
+# gcc-15 ParSec.c -o parsec -fopenmp
+# OMP_NUM_THREADS=4 ./parsec
+# rm parsec
+
+## 5:
+gcc-15 SumArray.c -o sumarray -fopenmp
+./sumarray
+rm sumarray
